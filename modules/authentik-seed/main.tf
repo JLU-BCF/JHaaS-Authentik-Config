bd85fc41054bef7ef8c2e12a280e0b8f1706479f
Nils Mittler <nils.mittler@computational.bio.uni-giessen.de>
2023-06-27 12:29:27 +0200

    Add enrollment

--- modules/authentik-seed/main.tf
+++ modules/authentik-seed/main.tf
@@ -249,6 +249,55 @@ resource "authentik_stage_prompt_field" "enrollment_mfa_text" {
 ########################
 #
 
+# Policy to set login redirect for jhaas as it gets lost in enrollment flow
+# when user navigates away to confirm the email address
+resource "authentik_policy_expression" "enrollment_login_redirect" {
+  name              = "jhaas-enrollment-login-redirect"
+  execution_logging = true
+  expression        = <<-LOGIN_REDIRECT
+      context['flow_plan'].context['redirect'] = "${var.authentik_jhaas_login_redirect}"
+      return True
+  LOGIN_REDIRECT
+}
+
+# Policy to check if username is available
+resource "authentik_policy_expression" "enrollment_check_username" {
+  name              = "jhaas-enrollment-check-username"
+  execution_logging = true
+  expression        = <<-CHECK_USERNAME
+      check_user_username = ak_user_by(username=context['prompt_data']['email'])
+      check_user_email = ak_user_by(email=context['prompt_data']['email'])
+
+      if check_user_username or check_user_email:
+        ak_message('This email address has already been taken, you may login instead.')
+        plan = request.context.get("flow_plan")
+        if not plan:
+          return False
+        plan.redirect("${var.authentik_jhaas_login_flow}")
+        return False
+
+      return True
+  CHECK_USERNAME
+}
+
+# Policy to map username and attributes
+resource "authentik_policy_expression" "enrollment_map_attributes" {
+  name              = "jhaas-enrollment-map-attributes"
+  execution_logging = true
+  expression        = <<-MAP_ATTRIBUTES
+      context['prompt_data']['username'] = context['prompt_data']['email']
+
+      context['prompt_data']['name'] = context['prompt_data']['givenname'] + ' ' + context['prompt_data']['familyname']
+
+      context['prompt_data']['attributes'] = {}
+      context['prompt_data']['attributes']['given_name'] = context['prompt_data']['givenname']
+      context['prompt_data']['attributes']['family_name'] = context['prompt_data']['familyname']
+
+
+      return True
+  MAP_ATTRIBUTES
+}
+
 #
 ########################
 #
@@ -295,6 +344,93 @@ resource "authentik_stage_prompt" "password_setup" {
 resource "authentik_stage_user_write" "password_setup" {
   name                = "jhaas-password-setup-write"
   user_creation_mode  = "never_create"
+}
+
+# Prompt Stage to show TOS and get acceptance
+resource "authentik_stage_prompt" "enrollment_tos" {
+  name = "jhaas-enrollment-tos"
+  fields = [
+    resource.authentik_stage_prompt_field.enrollment_tos_text.id,
+    resource.authentik_stage_prompt_field.enrollment_tos_accept.id
+  ]
+}
+
+# Prompt Stage to get enrollment user details
+resource "authentik_stage_prompt" "enrollment_user" {
+  name = "jhaas-enrollment-user"
+  fields = [
+    resource.authentik_stage_prompt_field.enrollment_given_name.id,
+    resource.authentik_stage_prompt_field.enrollment_family_name.id,
+    resource.authentik_stage_prompt_field.enrollment_email.id,
+    resource.authentik_stage_prompt_field.enrollment_password.id
+  ]
+}
+
+# Deny Stage for cancelling enrollment
+resource "authentik_stage_deny" "enrollment_cancel" {
+  name = "jhaas-enrollment-cancel"
+}
+
+# User Write Stage to save user
+resource "authentik_stage_user_write" "enrollment_write" {
+  name                      = "jhaas-enrollment-write"
+  create_users_as_inactive  = true
+  create_users_group        = authentik_group.auth_untrusted.id
+  user_creation_mode        = "always_create"
+}
+
+# Email Stage for email verification in enrollment
+resource "authentik_stage_email" "enrollment_email" {
+  name = "jhaas-enrollment-email"
+  use_global_settings = true
+  activate_user_on_success = true
+  subject = var.authentik_email_subject_enrollment
+  template = var.authentik_email_template_enrollment
+  token_expiry = 30
+}
+
+# Prompt Stage to show information before showing recovery codes
+resource "authentik_stage_prompt" "enrollment_pre_recovery_codes" {
+  name = "jhaas-enrollment-pre-recovery-codes"
+  fields = [
+    resource.authentik_stage_prompt_field.enrollment_recovery_codes_text.id
+  ]
+}
+
+# Stage to setup static recovery codes for enrollment
+resource "authentik_stage_authenticator_static" "enrollment_recovery_codes" {
+  name            = "jhaas-enrollment-recovery-codes"
+  configure_flow  = authentik_flow.mfa_static_setup.uuid
+  friendly_name   = "Static Recovery Codes"
+  token_count     = 6
+}
+
+# Prompt Stage to show information before setting up MFA
+resource "authentik_stage_prompt" "enrollment_pre_mfa" {
+  name = "jhaas-enrollment-pre-mfa"
+  fields = [
+    resource.authentik_stage_prompt_field.enrollment_mfa_text.id
+  ]
+}
+
+# Stage to validate (in terms of enrollment: setup) mfa
+resource "authentik_stage_authenticator_validate" "enrollment_mfa_setup" {
+  name                        = "jhaas-enrollment-mfa-setup"
+  device_classes              = ["totp", "webauthn"]
+  not_configured_action       = "configure"
+  webauthn_user_verification  = "required"
+  last_auth_threshold         = "seconds=0"
+  configuration_stages        = [
+    authentik_stage_authenticator_totp.totp_setup.id,
+    authentik_stage_authenticator_webauthn.webauthn_setup.id
+  ]
+}
+
+# Login Stage to automatically login user after enrollment
+resource "authentik_stage_user_login" "enrollment_login" {
+  name                = "jhaas-enrollment-login"
+  remember_me_offset  = "seconds=0"
+  session_duration    = "seconds=0"
 }
 
 #
@@ -360,6 +496,116 @@ resource "authentik_flow_stage_binding" "password_setup_2_password_setup_write" 
   evaluate_on_plan = false
 }
 
+# Binds TOS Stage to Enrollment Flow
+resource "authentik_flow_stage_binding" "enrollment_2_enrollment_tos" {
+  target = authentik_flow.enrollment.uuid
+  stage  = authentik_stage_prompt.enrollment_tos.id
+  order  = 10
+  invalid_response_action = "retry"
+  policy_engine_mode = "any"
+  re_evaluate_policies = true
+  evaluate_on_plan = false
+}
+
+# Binds User Prompt to Enrollment Flow
+resource "authentik_flow_stage_binding" "enrollment_2_enrollment_user" {
+  target = authentik_flow.enrollment.uuid
+  stage  = authentik_stage_prompt.enrollment_user.id
+  order  = 20
+  invalid_response_action = "retry"
+  policy_engine_mode = "any"
+  re_evaluate_policies = true
+  evaluate_on_plan = false
+}
+
+# Binds Deny Stage to Enrollment Flow
+resource "authentik_flow_stage_binding" "enrollment_2_enrollment_cancel" {
+  target = authentik_flow.enrollment.uuid
+  stage  = authentik_stage_deny.enrollment_cancel.id
+  order  = 25
+  invalid_response_action = "retry"
+  policy_engine_mode = "any"
+  re_evaluate_policies = true
+  evaluate_on_plan = false
+}
+
+# Binds User Write Stage to Enrollment Flow
+resource "authentik_flow_stage_binding" "enrollment_2_enrollment_write" {
+  target = authentik_flow.enrollment.uuid
+  stage  = authentik_stage_user_write.enrollment_write.id
+  order  = 30
+  invalid_response_action = "retry"
+  policy_engine_mode = "any"
+  re_evaluate_policies = true
+  evaluate_on_plan = false
+}
+
+# Binds Email Stage to Enrollment Flow
+resource "authentik_flow_stage_binding" "enrollment_2_enrollment_email" {
+  target = authentik_flow.enrollment.uuid
+  stage  = authentik_stage_email.enrollment_email.id
+  order  = 40
+  invalid_response_action = "retry"
+  policy_engine_mode = "any"
+  re_evaluate_policies = true
+  evaluate_on_plan = false
+}
+
+# Binds Pre Recovery Codes Stage to Enrollment Flow
+resource "authentik_flow_stage_binding" "enrollment_2_enrollment_pre_recovery_codes" {
+  target = authentik_flow.enrollment.uuid
+  stage  = authentik_stage_prompt.enrollment_pre_recovery_codes.id
+  order  = 45
+  invalid_response_action = "retry"
+  policy_engine_mode = "any"
+  re_evaluate_policies = true
+  evaluate_on_plan = false
+}
+
+# Binds Recovery Codes Stage to Enrollment Flow
+resource "authentik_flow_stage_binding" "enrollment_2_enrollment_recovery_codes" {
+  target = authentik_flow.enrollment.uuid
+  stage  = authentik_stage_authenticator_static.enrollment_recovery_codes.id
+  order  = 50
+  invalid_response_action = "retry"
+  policy_engine_mode = "any"
+  re_evaluate_policies = true
+  evaluate_on_plan = false
+}
+
+# Binds Pre MFA Stage to Enrollment Flow
+resource "authentik_flow_stage_binding" "enrollment_2_enrollment_pre_mfa" {
+  target = authentik_flow.enrollment.uuid
+  stage  = authentik_stage_prompt.enrollment_pre_mfa.id
+  order  = 55
+  invalid_response_action = "retry"
+  policy_engine_mode = "any"
+  re_evaluate_policies = true
+  evaluate_on_plan = false
+}
+
+# Binds MFA Setup Stage to Enrollment Flow
+resource "authentik_flow_stage_binding" "enrollment_2_enrollment_mfa_setup" {
+  target = authentik_flow.enrollment.uuid
+  stage  = authentik_stage_authenticator_validate.enrollment_mfa_setup.id
+  order  = 60
+  invalid_response_action = "retry"
+  policy_engine_mode = "any"
+  re_evaluate_policies = true
+  evaluate_on_plan = false
+}
+
+# Binds Login Stage to Enrollment Flow
+resource "authentik_flow_stage_binding" "enrollment_2_enrollment_login" {
+  target = authentik_flow.enrollment.uuid
+  stage  = authentik_stage_user_login.enrollment_login.id
+  order  = 70
+  invalid_response_action = "retry"
+  policy_engine_mode = "any"
+  re_evaluate_policies = true
+  evaluate_on_plan = true
+}
+
 #
 ########################
 #
@@ -367,3 +613,33 @@ resource "authentik_flow_stage_binding" "password_setup_2_password_setup_write" 
 #
 ########################
 #
+
+# Binds Login Redirect Policy to enrollment_2_enrollment_login binding
+resource "authentik_policy_binding" "enrollment_login_redirect_2_enrollment" {
+  target = authentik_flow_stage_binding.enrollment_2_enrollment_login.id
+  policy = authentik_policy_expression.enrollment_login_redirect.id
+  order  = 0
+  enabled = true
+  negate = false
+  timeout = 30
+}
+
+# Binds Check Username Policy to enrollment_2_enrollment_cancel
+resource "authentik_policy_binding" "enrollment_check_username_2_enrollment" {
+  target = authentik_flow_stage_binding.enrollment_2_enrollment_cancel.id
+  policy = authentik_policy_expression.enrollment_check_username.id
+  order  = 10
+  enabled = true
+  negate = true
+  timeout = 30
+}
+
+# Binds Map Attributes Policy to enrollment_2_enrollment_cancel
+resource "authentik_policy_binding" "enrollment_map_attributes_2_enrollment" {
+  target = authentik_flow_stage_binding.enrollment_2_enrollment_cancel.id
+  policy = authentik_policy_expression.enrollment_map_attributes.id
+  order  = 20
+  enabled = true
+  negate = true
+  timeout = 30
+}
--- modules/authentik-seed/variables.tf
+++ modules/authentik-seed/variables.tf
@@ -5,3 +5,21 @@ variable "authentik_flow_background" {
 variable "authentik_tos_url" {
   description = "URL linked in TOS text"
 }
+
+variable "authentik_jhaas_login_flow" {
+  description = "URL for the JHaaS authentication flow"
+  # e.g. https://auth.jhaas.gi.denbi.de/if/flow/jhaas-authentication
+}
+
+variable "authentik_jhaas_login_redirect" {
+  description = "URL for the JHaaS login redirect"
+  # e.g. https://jhaas-test.gi.denbi.de/api/auth/oidc/cb
+}
+
+variable "authentik_email_subject_enrollment" {
+  default = "Verify you Email address for JHaaS"
+}
+
+variable "authentik_email_template_enrollment" {
+  default = "email/account_confirmation.html"
+}
