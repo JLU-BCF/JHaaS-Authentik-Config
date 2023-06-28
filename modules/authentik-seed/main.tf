bf221121267bbef73f531aabf24fd92bba09fc0f
Nils Mittler <nils.mittler@computational.bio.uni-giessen.de>
2023-06-28 14:45:08 +0200

    create mappings, provider, app and tenant

--- modules/authentik-seed/main.tf
+++ modules/authentik-seed/main.tf
@@ -1,5 +1,5 @@
 provider "authentik" {
-  url = var.authentik_url
+  url   = var.authentik_url
   token = var.authentik_token
 }
 
@@ -992,4 +992,153 @@ resource "authentik_policy_binding" "logout_redirect_if_unauth_2_logout" {
   enabled = true
   negate = false
   timeout = 30
+}
+
+#
+########################
+#
+# Add Mappings
+#
+########################
+#
+
+# Map email attributes
+resource "authentik_scope_mapping" "email" {
+  name        = "jhaas-email"
+  scope_name  = "email"
+  description = "Map Email address"
+  expression  = <<-SCOPE_EMAIL
+      return {
+          "email": request.user.email,
+          "email_verified": True
+      }
+  SCOPE_EMAIL
+}
+
+# Map openid attributes
+resource "authentik_scope_mapping" "openid" {
+  name        = "jhaas-openid"
+  scope_name  = "openid"
+  description = "Map openid"
+  expression  = <<-SCOPE_OPENID
+      return {}
+  SCOPE_OPENID
+}
+
+# Map profile attributes
+resource "authentik_scope_mapping" "profile" {
+  name        = "jhaas-profile"
+  scope_name  = "profile"
+  description = "Map profile"
+  expression  = <<-SCOPE_PROFILE
+      return {
+          "name": request.user.name,
+          "given_name": request.user.attributes.given_name or request.user.name,
+          "family_name": request.user.attributes.family_name or request.user.name,
+          "preferred_username": request.user.email,
+          "nickname": request.user.given_name,
+          "groups": [group.name for group in request.user.ak_groups.all()],
+      }
+  SCOPE_PROFILE
+}
+
+# Map user attributes
+resource "authentik_scope_mapping" "user_attributes" {
+  name        = "jhaas-user-attributes"
+  scope_name  = "user-attributes"
+  description = "Map user attributes into OIDC response"
+  expression  = <<-USER_ATTRIBUTES
+      return request.user.attributes
+  USER_ATTRIBUTES
+}
+
+#
+########################
+#
+# Add Provider
+#
+########################
+#
+
+# The JHaaS Portal OAuth Provider
+resource "authentik_provider_oauth2" "portal" {
+  name      = "jhaas-portal"
+
+  client_id = var.authentik_jhaas_client_id
+  client_secret = var.authentik_jhaas_client_secret
+  client_type = "confidential"
+  redirect_uris = [
+    var.authentik_redirect_uri
+  ]
+
+  sub_mode = "user_id"
+
+  authentication_flow = authentik_flow.auth.uuid
+  authorization_flow  = authentik_flow.consent.uuid
+
+  access_code_validity = "minutes=1"
+  access_token_validity = "minutes=5"
+  refresh_token_validity = "days=30"
+  include_claims_in_id_token  = true
+  issuer_mode = "per_provider"
+
+  property_mappings = [
+    authentik_scope_mapping.email.id,
+    authentik_scope_mapping.openid.id,
+    authentik_scope_mapping.profile.id,
+    authentik_scope_mapping.user_attributes.id
+  ]
+}
+
+#
+########################
+#
+# Add Application
+#
+########################
+#
+
+# The JHaaS Portal Application
+resource "authentik_application" "portal" {
+  name              = "jhaas-portal"
+  slug              = "portal"
+  protocol_provider = authentik_provider_oauth2.portal.id
+
+  backchannel_providers = []
+  open_in_new_tab = false
+  policy_engine_mode = "any"
+  group = ""
+
+  meta_description = var.authentik_jhaas_slogan
+  meta_icon = var.authentik_branding_favicon
+  meta_launch_url = var.authentik_jhaas_launch_url
+  meta_publisher = var.authentik_branding_publisher
+}
+
+#
+########################
+#
+# Add Tenant
+#
+########################
+#
+
+resource "authentik_tenant" "jhaas" {
+  domain              = var.authentik_domain
+  default             = true
+
+  branding_title      = var.authentik_branding_title
+  branding_favicon    = var.authentik_branding_favicon
+  branding_logo       = var.authentik_branding_logo
+
+  flow_authentication = authentik_flow.auth.uuid
+  flow_device_code    = ""
+  flow_invalidation   = authentik_flow.logout.uuid
+  flow_recovery       = authentik_flow.recovery.uuid
+  flow_unenrollment   = ""
+  flow_user_settings  = ""
+
+  web_certificate     = ""
+  event_retention     = "days=365"
+  attributes          = {}
 }
--- modules/authentik-seed/variables.tf
+++ modules/authentik-seed/variables.tf
@@ -1,3 +1,15 @@
+variable "authentik_jhaas_client_id" {
+
+}
+
+variable "authentik_jhaas_client_secret" {
+
+}
+
+variable "authentik_provider_redirect_uri" {
+  # e.g. https?://jhaas-test\.gi\.denbi\.de/.*
+}
+
 variable "authentik_flow_background" {
   description = "Default Background applied to all flows"
 }
@@ -8,6 +20,7 @@ variable "authentik_tos_url" {
 
 variable "authentik_jhaas_login_flow" {
   description = "URL for the JHaaS authentication flow"
+  default = "/if/flow/auth"
   # e.g. https://auth.jhaas.gi.denbi.de/if/flow/jhaas-authentication
 }
 
@@ -19,6 +32,16 @@ variable "authentik_jhaas_login_redirect" {
 variable "authentik_jhaas_verify_redirect" {
   description = "URL for the JHaaS verify redirect"
   # e.g. https://jhaas-test.gi.denbi.de/verify
+}
+
+variable "authentik_jhaas_launch_url" {
+  description = "URL for the JHaaS launcher redirect"
+  # e.g. https://jhaas-test.gi.denbi.de/api/auth/oidc/login
+}
+
+variable "authentik_jhaas_slogan" {
+  description = "Slogan displayed for the JHaaS launcher"
+  default = "Create your personal Jupyter Hub instance on the go"
 }
 
 variable "authentik_email_subject_enrollment" {
@@ -35,4 +58,23 @@ variable "authentik_email_subject_recovery" {
 
 variable "authentik_email_template_recovery" {
   default = "email/password_reset.html"
+}
+
+variable "authentik_branding_title" {
+  default = "JHaaS"
+}
+
+variable "authentik_branding_favicon" {
+  default = "/static/dist/assets/icons/icon.png"
+}
+
+variable "authentik_branding_logo" {
+  default = "/static/dist/assets/icons/icon_left_brand.svg"
+}
+
+variable "authentik_branding_publisher" {
+  default = ""
+}
+
+variable "authentik_domain" {
 }
