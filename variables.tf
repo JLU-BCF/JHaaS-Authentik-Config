e8acdabb651469a8432e4e484d6ed8463e95f561
Nils Mittler <nils.mittler@computational.bio.uni-giessen.de>
2023-07-19 11:10:53 +0200

    scope repo to single module, outsource deployment

--- variables.tf
+++ variables.tf
@@ -1,59 +1,134 @@
-######################
-# Deployment configuration
-######################
-
-variable "kubeconfig_path" {
-  description = "Path to kubernetes configuration file to use"
-  default = "~/.kube/config"
-}
-variable "k8s_namespace" {
-  description = "The kubernetes namespace to deploy authentik in"
-  default = "jhaas-authentik"
-}
-variable "release_name" {
-  description = "The release name for the helm deployment"
-  default = "jhaas-authentik"
+# Configure the terraform provider
+variable "authentik_token" {
+  description = "Authentic token that will be used for API calls."
+  default = "tokentokentokentokentokentokentokentokentokentoken"
 }
 
-######################
-# Authentik configuration
-######################
-
-variable "authentik_version" {
-  description = "Authentik version, used as chart version and image tag"
-}
-variable "authentik_hostname" {
-  description = "Hostname under which authentik will be deployed"
-}
-variable "authentik_email" {
-  description = "Email address injected as email address for akadmin"
+# Configure authentik Accessibility
+variable "authentik_domain" {
+  description = "Domain where Authentik is accessible."
+  default = "localhost"
 }
 
-######################
-# Database configuration
-######################
-
-variable "postgresql_enabled" {
-  description = "Controls if the buildin postgres chart should be deployed"
-  default = true
+variable "authentik_port" {
+  description = "Port where authentik is accessible. Set null to omit a port and use defaults (80/443)."
+  default = 9000
 }
 
-######################
-# Ingress configuration
-######################
+variable "authentik_ssl" {
+  description = "Control use of https (true) oder http (null)."
+  default = null
+}
 
-variable "use_tls" {
-  description = "Control if tls is used by ingress"
+variable "authentik_path" {
+  description = "Path where Authentik is accessible."
+  default = ""
 }
-variable "ingress_use_tls_acme" {
-  description = "Sets the kubernetes.io/tls-acme annotation for ingress"
-  default = true
+
+# Configure JHaaS Accessibility
+variable "jhaas_domain" {
+  description = "The domain where JHaaS is accessible."
+  default = "localhost"
 }
-variable "ingress_tls_secret_name" {
-  description = "Sets the secret name for tls for ingress"
-  default = "jhaas-authentik-tls"
+
+variable "jhaas_port" {
+  description = "Port where JHaaS is accessible. Set null to omit a port and use defaults (80/443)."
+  default = 3000
 }
-variable "ingress_cert_manager_issuer" {
-  description = "Sets the cert-manager.io/cluster-issuer annotation for ingress"
-  default = "cert-manager"
+
+variable "jhaas_ssl" {
+  description = "Control use of https (true) oder http (null)."
+  default = null
+}
+
+variable "jhaas_path" {
+  description = "Path where JHaaS is accessible."
+  default = ""
+}
+
+# Configure the OAuth Provider for JHaaS Portal
+variable "authentik_jhaas_client_id" {
+  description = "The client_id for the default jhaas provider."
+  default = "jhaas-portal"
+}
+
+variable "authentik_jhaas_client_secret" {
+  description = "Secret to be set for the jhaas provider."
+  default = "mysupersecretclientsecretmysupersecretclientsecret"
+}
+
+variable "authentik_provider_redirect_uri" {
+  description = "Allowed redirection URLs for jhaas provider."
+  default = null
+}
+
+# Configure Flows
+variable "authentik_jhaas_login_flow" {
+  description = "URL for the JHaaS authentication flow."
+  default = "/if/flow/auth"
+}
+
+variable "authentik_flow_background" {
+  description = "Default Background applied to all flows."
+  default = "/static/dist/assets/images/flow_background.jpg"
+}
+
+variable "authentik_tos_url" {
+  description = "URL linked in TOS text"
+  default = null
+}
+
+variable "authentik_jhaas_login_redirect" {
+  description = "URL for the JHaaS login redirect."
+  default = null
+}
+
+variable "authentik_jhaas_verify_redirect" {
+  description = "URL for the JHaaS verify redirect."
+  default = null
+}
+
+variable "authentik_jhaas_launch_url" {
+  description = "URL for the JHaaS launcher redirect."
+  default = null
+}
+
+# Configure Application
+variable "authentik_jhaas_slogan" {
+  description = "Slogan displayed for the JHaaS launcher."
+  default = "Create your personal Jupyter Hub instance on the go"
+}
+
+variable "authentik_branding_title" {
+  description = "Title displayed by the web application."
+  default = "JHaaS"
+}
+
+variable "authentik_branding_favicon" {
+  default = "/static/dist/assets/icons/icon.png"
+}
+
+variable "authentik_branding_logo" {
+  default = "/static/dist/assets/icons/icon_left_brand.svg"
+}
+
+variable "authentik_branding_publisher" {
+  default = ""
+}
+
+# Configure Mails
+variable "authentik_email_subject_enrollment" {
+  default = "Verify you Email address for JHaaS"
+}
+
+variable "authentik_email_template_enrollment" {
+  default = "email/account_confirmation.html"
+}
+
+variable "authentik_email_subject_recovery" {
+  default = "Reset your password for JHaaS"
+}
+
+variable "authentik_email_template_recovery" {
+  default = "email/password_reset.html"
 }
