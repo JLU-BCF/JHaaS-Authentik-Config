51e12b72b3ce08733f6ac9ef38f7b17de432045e
Nils Mittler <nils.mittler@computational.bio.uni-giessen.de>
2023-09-07 13:02:19 +0200

    inherit provider config

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-provider "authentik" {
-  url   = local.authentik_url
-  token = var.authentik_token
-}
+# provider "authentik" {
+#   url   = local.authentik_url
+#   token = var.authentik_token
+# }
 
 #
 ########################
